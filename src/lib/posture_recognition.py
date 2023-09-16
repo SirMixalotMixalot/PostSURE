33c3c7aadabf169d389173002dc61a7a1cc0374d
bk <bkmogomo@uwaterloo.ca>
2023-09-16 04:31:17 -0400

    lmao

--- src/lib/posture_recognition.py
+++ src/lib/posture_recognition.py
@@ -21,7 +21,6 @@ def get_contour_mask():
     camera = cv2.VideoCapture(0)
     _, image = camera.read()
 
-<<<<<<< HEAD
     k = min(1.0, 1024/max(image.shape[0], image.shape[1]))
     image = cv2.resize(image, None, fx=k, fy=k, interpolation=cv2.INTER_LANCZOS4)
 
@@ -40,32 +39,3 @@ def get_contour_mask():
     mask = apply_deeplab(deeplab, image, device)
 
     return mask
-=======
-    # find the contours from the thresholded image
-    contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
-
-
-
-def get_basic_contours3(myimage):
-    # BG Remover 3
-    myimage_hsv = cv2.cvtColor(myimage, cv2.COLOR_BGR2HSV)
-     
-    #Take S and remove any value that is less than half
-    s = myimage_hsv[:,:,1]
-    s = np.where(s < 127, 0, 1) # Any value below 127 will be excluded
- 
-    # We increase the brightness of the image and then mod by 255
-    v = (myimage_hsv[:,:,2] + 127) % 255
-    v = np.where(v > 127, 1, 0)  # Any value above 127 will be part of our mask
- 
-    # Combine our two masks based on S and V into a single "Foreground"
-    foreground = np.where(s+v > 0, 1, 0).astype(np.uint8)  #Casting back into 8bit integer
- 
-    background = np.where(foreground==0,255,0).astype(np.uint8) # Invert foreground to get background in uint8
-    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)  # Convert background back into BGR space
-    foreground=cv2.bitwise_and(myimage,myimage,mask=foreground) # Apply our foreground map to original image
-    finalimage = background+foreground # Combine foreground and background
- 
-    return finalimage
->>>>>>> d495eda24311a2e53db0599f6b0cf836e04083fc
